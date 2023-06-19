63a15fdff5c63eed74927cdf85c3d2bfb20b01de
shurintou <shurintou@gmail.com>
2023-06-19 17:29:36 +0900

    feature(traverseDirectory): to traverse a certain directory to count the js function lines. chore(your_file.js): to remove the your_file.js.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 const fs = require('fs')
+const path = require('path')
 const jsFunctionLineCount = require('./jsFunctionLineCount')
 const COUNT_COMMENT = false
-const TARGET_PATH = 'your_file.js'
+const TARGET_PATH = 'D:/Programs/my-code-memos/src'
 
-const functionLineCountsResult = jsFunctionLineCount(TARGET_PATH, COUNT_COMMENT)
-
-// output the result
 let outputStr = ''
-for (const functionName in functionLineCountsResult) {
-    outputStr += `The lines of function '${functionName}' is：${functionLineCountsResult[functionName]}\n`
+
+function traverseDirectory(directoryPath) {
+    if (!path.isAbsolute(directoryPath)) directoryPath = __dirname + '\\' + directoryPath
+    const stats = fs.statSync(directoryPath)
+    if (stats.isDirectory()) {
+        const files = fs.readdirSync(directoryPath)
+        files.forEach((file) => {
+            const filePath = path.join(directoryPath, file)
+            traverseDirectory(filePath)
+        })
+    } else {
+        if (path.extname(directoryPath) === '.js') {
+            const functionLineCountsResult = jsFunctionLineCount(directoryPath, COUNT_COMMENT)
+            if (Object.keys(functionLineCountsResult).length > 0) {
+                outputStr += directoryPath + '\n'
+                // output the result
+                for (const functionName in functionLineCountsResult) {
+                    outputStr += `The lines of function '${functionName}' is：${functionLineCountsResult[functionName]}\n`
+                }
+                outputStr += '\n'
+            }
+        }
+    }
 }
+
+traverseDirectory(TARGET_PATH)
 fs.writeFileSync('result.txt', outputStr)
