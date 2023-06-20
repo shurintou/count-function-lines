8e45c40148a83ea0cd41c0a50261bf9e85935a77
shurintou <shurintou@gmail.com>
2023-06-20 22:42:34 +0900

    chore(comment): to add comments to config.js.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,16 @@
 module.exports = {
+    // Whether to count comment or not, the false is not to count.
     countComment: false,
+    // Functions whose count line is less than this value will not output. 
     minLineCount: 0,
+    // Functions whose count line is larger than this value will not output. 
     maxLineCount: Infinity,
+    // The path of folder or file that you want to count lines.
     targetPath: 'D:/Programs/my-code-memos',
+    // The regular expressions of folder or file that you don't want to count lines.
     excludePaths: [/.*node_modules.*/, /.*jquery.*/],
+    // Where the result file to be output.
     ouputResultFilePath: 'result.txt',
+    // The template of the output.
     outputTemplate: (functionName, linesCount, startLine, endLine) => `The lines of function '${functionName}'(${startLine}:${endLine}) is：${linesCount}\n`,
 }
