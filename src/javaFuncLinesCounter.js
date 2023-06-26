9f535164dcbfc401542e015fa110b76fb52cfbd2
shurintou <shurintou@gmail.com>
2023-06-27 01:52:39 +0900

    feature(javaFuncLinesCounter.js): to complete the feature of javaFuncLinesCounter.

--- src/javaFuncLinesCounter.js
+++ src/javaFuncLinesCounter.js
@@ -1,5 +1,22 @@
 const fs = require('fs')
 const { parse, BaseJavaCstVisitorWithDefaults } = require("java-parser")
+const config = require('../config')
+const { countComment, countBlank, minLineCount, maxLineCount, excludeFunctionNames } = config
+
+/**
+ * @typedef FunctionInnerComment
+ * @type {object}
+ * @property {string} image - The content of the comment.
+ * @property {number} startLine -  The start line of the comment. 
+ * @property {number} endLine - The end line of the comment.
+ */
+
+/**
+ * @typedef FunctionLocation
+ * @type {object}
+ * @property {number} startLine -  The start line of the function. 
+ * @property {number} endLine - The end line of the function.
+ */
 
 /**
  * @typedef {import('./funcLinesCountHandler').FunctionLineCountsResult} FunctionLineCountsResult
@@ -22,8 +39,17 @@ class MethodVisitor extends BaseJavaCstVisitorWithDefaults {
     constructor() {
         super()
         this.validateVisitor()
+        /**
+         * @type {string[]} 
+         */
         this.methodNames = []
+        /**
+         * @type {FunctionLocation[]} 
+         */
         this.methodLocations = []
+        /**
+        * @type {FunctionInnerComment[]} 
+        */
         this.methodComments = []
     }
 
@@ -48,8 +74,6 @@ class MethodVisitor extends BaseJavaCstVisitorWithDefaults {
         this.methodComments.push(ctx.leadingComments)
     }
 
-
-
 }
 
 /**
@@ -58,21 +82,67 @@ class MethodVisitor extends BaseJavaCstVisitorWithDefaults {
  */
 const javaFuncCounter = function (fileContent) {
     const cst = parse(fileContent)
+    /**
+     * @type {string[]} 
+     */
+    const lines = fileContent.split('\n')
     const visitor = new MethodVisitor()
     /** 
      * @type {FunctionLineCountsResult[]}
      */
     let functionLineCountsResult = []
+
     visitor.visit(cst)
-    visitor.methodNames.forEach((name, index) => {
-        const { startLine, endLine } = visitor.methodLocations[index]
-        functionLineCountsResult.push({
-            functionName: name,
-            lineCount: endLine - startLine + 1,
-            startLine: startLine,
-            endLine: endLine,
+
+    if (visitor.methodNames) {
+        visitor.methodNames.forEach((name, index) => {
+            const { startLine, endLine } = visitor.methodLocations[index]
+            countLines(startLine, endLine, name, visitor.methodComments[index] || [])
         })
-    })
+    }
+
+    /**
+     * This is the function to count the line and push the result to the results list. 
+     * @param {number} startLine The startLine of the function.
+     * @param {number} endLine The endLine of the function.
+     * @param {string} functionName The name of the function.
+     * @param {FunctionInnerComment[]} functionComments The comments list of the function.
+     */
+    function countLines(startLine, endLine, functionName, functionComments) {
+        if (excludeFunctionNames.some(regex => regex.test(functionName))) return
+        let lineCount = 0
+
+        // to cumulate the lines count
+        for (let i = startLine - 1; i < endLine; i++) {
+            const lineStr = lines[i].trim()
+            if (lineStr !== '' || countBlank) {
+                lineCount = lineCount + 1
+            }
+        }
+
+        // to subtract the lines count of comments from the count result above
+        functionComments.forEach(comment => {
+            if (countComment) return
+            const { startLine, endLine, image } = comment
+            if (startLine !== endLine) {
+                lineCount = lineCount - (endLine - startLine + 1)
+            }
+            else if (image === lines[startLine - 1].trim()) {
+                lineCount = lineCount - 1
+            }
+        })
+
+        // to push the result to the results list
+        if (minLineCount <= lineCount && lineCount <= maxLineCount) {
+            functionLineCountsResult.push({
+                functionName: functionName,
+                lineCount: lineCount,
+                startLine: startLine,
+                endLine: endLine,
+            })
+        }
+
+    }
     return functionLineCountsResult
 }
 
