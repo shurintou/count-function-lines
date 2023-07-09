62f5f756195e70d682360b1f85fbf9d793ebdad0
shurintou <shurintou@gmail.com>
2023-07-10 01:40:20 +0900

    feature(progress bar): to print progress bar in console.

--- src/funcLinesCountHandler.js
+++ src/funcLinesCountHandler.js
@@ -5,6 +5,7 @@ const { vueFuncCounterHandler } = require('./vueFuncLinesCounter')
 const { javaFuncCounterHandler } = require('./javaFuncLinesCounter')
 const config = require('../config')
 const { targetPath, excludePaths } = config
+const supportFileType = ['.js', '.jsx', '.ts', '.tsx', '.vue', 'java']
 
 /**
  * @typedef FunctionLineCountsResult
@@ -21,6 +22,20 @@ const { targetPath, excludePaths } = config
  */
 const funcLinesCountHandler = function () {
     let outputStr = ''
+    /**
+     * @type {string[]}  
+     */
+    const filePathList = []
+    traverseDirectory(targetPath)
+    const filePathListLenght = filePathList.length
+    console.log('Start counting...')
+    filePathList.forEach((filePath, index) => {
+        showProgressBar(filePathListLenght, index + 1)
+        const resultStr = countFunctionLinesOfFile(filePath)
+        outputStr += resultStr
+    })
+    console.log('Counting Finished.')
+
 
     /** 
      * Traverse the directoryPath to invoke function count handlers.
@@ -37,37 +52,74 @@ const funcLinesCountHandler = function () {
                 traverseDirectory(filePath)
             })
         } else {
-            const fileExtname = path.extname(directoryPath)
-            /** 
-             * @type {FunctionLineCountsResult[]}
-             */
-            let functionLineCountsResult = []
-            try {
-                if (['.js', '.jsx', '.ts', '.tsx'].includes(fileExtname)) {
-                    functionLineCountsResult = jsFuncCounterHandler(directoryPath)
-                }
-                else if (fileExtname === '.vue') {
-                    functionLineCountsResult = vueFuncCounterHandler(directoryPath)
-                }
-                else if (fileExtname === '.java') {
-                    functionLineCountsResult = javaFuncCounterHandler(directoryPath)
-                }
-            }
-            catch (e) {
-                console.error(config.errorTemplate(e, directoryPath))
-            }
-            if (functionLineCountsResult.length > 0) {
-                outputStr += directoryPath + '\n'
-                // output the result
-                functionLineCountsResult.forEach(result => outputStr += config.outputTemplate(result.functionName, result.lineCount, result.startLine, result.endLine) + '\n')
-                outputStr += '\n'
-            }
+            if (supportFileType.includes(path.extname(directoryPath))) filePathList.push(directoryPath)
         }
     }
 
-    traverseDirectory(targetPath)
+
+    /** 
+     * Count function lines of the given file and return the result.
+     * @param {string} filePath
+     * @returns {string} The result.
+     */
+    function countFunctionLinesOfFile(filePath) {
+        let resultStr = ''
+
+        const fileExtname = path.extname(filePath)
+        /** 
+         * @type {FunctionLineCountsResult[]}
+         */
+        let functionLineCountsResult = []
+        try {
+            if (['.js', '.jsx', '.ts', '.tsx'].includes(fileExtname)) {
+                functionLineCountsResult = jsFuncCounterHandler(filePath)
+            }
+            else if (fileExtname === '.vue') {
+                functionLineCountsResult = vueFuncCounterHandler(filePath)
+            }
+            else if (fileExtname === '.java') {
+                functionLineCountsResult = javaFuncCounterHandler(filePath)
+            }
+        }
+        catch (e) {
+            console.error(config.errorTemplate(e, filePath))
+        }
+        if (functionLineCountsResult.length > 0) {
+            resultStr += filePath + '\n'
+            // append the result
+            functionLineCountsResult.forEach(result => resultStr += config.outputTemplate(result.functionName, result.lineCount, result.startLine, result.endLine) + '\n')
+            resultStr += '\n'
+        }
+
+        return resultStr
+    }
+
+
+    /** 
+     * To show process in console.
+     * @param {number} totalSteps
+     * @param {number} currentStep
+     */
+    function showProgressBar(totalSteps, currentStep) {
+        const progressBarLength = 40
+        const progress = currentStep / totalSteps
+        const filledBarLength = Math.round(progress * progressBarLength)
+        const emptyBarLength = progressBarLength - filledBarLength
+        const filledBar = "█".repeat(filledBarLength)
+        const emptyBar = "░".repeat(emptyBarLength)
+        const percentageProgress = Math.round(progress * 100)
+
+        process.stdout.write(`Progress: [${filledBar}${emptyBar}] ${percentageProgress}% (${currentStep}/${totalSteps})  \r`)
+
+        if (percentageProgress === 100) process.stdout.write('\n')
+
+    }
 
     return outputStr
 }
+
+
+
+
 
 module.exports = funcLinesCountHandler
