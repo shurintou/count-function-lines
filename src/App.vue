ca2404db79dfd5f1b779e0cffe8489c7000b29ee
shurintou <shurintou@gmail.com>
2023-08-25 00:23:24 +0900

    feature: import element ui plus and dark mode.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
 </script>
 
-<template></template>
+<template>
+    <el-button @click="toggleDark()">Default</el-button>
+</template>
 
 <style scoped></style>
