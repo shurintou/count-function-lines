041ac9630aa4cf58d604e4cc48fc212691afa76b
shurintou <shurintou@gmail.com>
2023-09-01 02:05:08 +0900

    feature: to add function getFuncCounter.
    # Conflicts:
    #	src/funcLinesCountHandler.js

---
+++ src/funcLinesCountHandler.js
@@ -0,0 +1,22 @@
+import jsFuncCounter from './counters/jsFuncLinesCounter.js'
+import vueFuncCounter from './counters/vueFuncLinesCounter.js'
+import javaFuncCounter from './counters/javaFuncLinesCounter.js'
+export const supportFileType = ['.js', '.jsx', '.ts', '.tsx', '.vue', '.java']
+
+/** 
+ * To get the function counter for a certain file extension.
+ * @param {string} fileExtname
+ * @return {((fileContent: string, minLineCount?: number | undefined, maxLineCount?: number | undefined, excludeFunctionNames?: RegExp[] | undefined, offset?: number | undefined) => FunctionLineCountsResult[]) | undefined} 
+ */
+export const getFuncCounter = (fileExtname) => {
+    if (['.js', '.jsx', '.ts', '.tsx'].includes(fileExtname)) {
+        return jsFuncCounter
+    }
+    else if (fileExtname === '.vue') {
+        return vueFuncCounter
+    }
+    else if (fileExtname === '.java') {
+        return javaFuncCounter
+    }
+    return undefined
+}
