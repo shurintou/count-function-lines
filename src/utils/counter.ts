ff0c5c9d30e82712e33a6dae2eb2f5436d180f9a
shurintou <shurintou@gmail.com>
2023-08-31 23:00:15 +0900

    feature: 1. to add watchers to count code each time the code be changed. 2. to add error display to CountResult.

--- src/utils/counter.ts
+++ src/utils/counter.ts
@@ -1,21 +1,47 @@
-import { ref, onMounted } from 'vue'
-import { jsFuncCounter, type FunctionLineCountsResult } from 'count-function-lines'
+import { ref, onMounted, computed, watch } from 'vue'
+import { jsFuncCounter, vueFuncCounter, javaFuncCounter, type FunctionLineCountsResult } from 'count-function-lines'
 import type { CountResult } from '@/types/index'
 import { displayCode } from '@/configs/constant'
 import { type SupportLanguages } from '@/types/index'
+import { throttle } from '@/utils/common'
 
 const code = ref<string>(displayCode)
+const errorMsg = ref<string>('')
 const language = ref<SupportLanguages>('.js')
 const tableData = ref<CountResult[]>([])
 
 export function useCounter() {
+    const counter = computed(() => {
+        if (['.js', '.jsx', '.ts', '.tsx'].includes(language.value)) {
+            return jsFuncCounter
+        }
+        else if (language.value === '.vue') {
+            return vueFuncCounter
+        }
+        else if (language.value === '.java') {
+            return javaFuncCounter
+        }
+        return jsFuncCounter
+    })
 
-    onMounted(() => {
-        const countResults: FunctionLineCountsResult[] = jsFuncCounter(code.value)
-        tableData.value = countResults.map(countResult => {
+    onMounted(() => countFunctionLine())
+    watch(counter, () => countFunctionLine())
+    watch(code, () => countFunctionLine())
+
+    const countFunctionLine = throttle(() => {
+        let countResultList: FunctionLineCountsResult[] = []
+        try {
+            countResultList = counter.value(code.value)
+            errorMsg.value = ''
+        }
+        catch (e) {
+            if (e instanceof Error) errorMsg.value = e.message
+        }
+        tableData.value = countResultList.map(countResult => {
             return { totalLineCount: countResult.endLine - countResult.startLine + 1, ...countResult }
         })
     })
 
-    return { code, language, tableData }
+
+    return { code, errorMsg, language, tableData }
 }
