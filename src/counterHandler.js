324efa66fc0a9570bffbc1a27e30d1906c7b173e
shurintou <shurintou@gmail.com>
2023-09-04 00:07:32 +0900

    refactor: to remove funcCounter code and import from main project.

--- src/counterHandler.js
+++ src/counterHandler.js
@@ -1,9 +1,7 @@
 import fs from 'fs'
 import * as path from 'path'
 import { fileURLToPath } from 'url'
-import jsFuncCounter from './counters/jsFuncLinesCounter.js'
-import vueFuncCounter from './counters/vueFuncLinesCounter.js'
-import javaFuncCounter from './counters/javaFuncLinesCounter.js'
+import { getFuncCounter } from 'count-function-lines'
 import config from '../config.js'
 const { minLineCount, maxLineCount, excludeFunctionNames, targetPath, excludePaths } = config
 export const supportFileExtensions = ['.js', '.jsx', '.ts', '.tsx', '.vue', '.java']
@@ -120,23 +118,5 @@ const counterHandler = function () {
     return outputStr
 }
 
-
-/** 
- * To get the function counter for a certain file extension.
- * @param {string} fileExtname
- * @return {((fileContent: string, minLineCount?: number | undefined, maxLineCount?: number | undefined, excludeFunctionNames?: RegExp[] | undefined) => FunctionLineCountsResult[]) | undefined} 
- */
-export const getFuncCounter = (fileExtname) => {
-    if (['.js', '.jsx', '.ts', '.tsx'].includes(fileExtname)) {
-        return jsFuncCounter
-    }
-    else if (fileExtname === '.vue') {
-        return vueFuncCounter
-    }
-    else if (fileExtname === '.java') {
-        return javaFuncCounter
-    }
-    return undefined
-}
 
 export default counterHandler
